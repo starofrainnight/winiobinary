af43d941e538092c56f12b6cdce610ddef035e68
Hong-She Liang <starofrainnight@gmail.com>
2018-01-07 21:55:40 +0800

    Initial package structure

---
+++ README.rst
@@ -0,0 +1,35 @@
+============
+winio-binary
+============
+
+
+.. image:: https://img.shields.io/pypi/v/winiobinary.svg
+        :target: https://pypi.python.org/pypi/winiobinary
+
+.. image:: https://img.shields.io/travis/starofrainnight/winiobinary.svg
+        :target: https://travis-ci.org/starofrainnight/winiobinary
+
+.. image:: https://readthedocs.org/projects/winiobinary/badge/?version=latest
+        :target: https://winiobinary.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/starofrainnight/winiobinary/shield.svg
+     :target: https://pyup.io/repos/github/starofrainnight/winiobinary/
+     :alt: Updates
+
+
+WinIo's binary python package that support install from pypi
+
+License
+---------
+
+This package is license under two different style license with different part.
+
+The package except WinIo binary that under 'data' directory are licensed under
+Apache-2.0, but the WinIo binary use it's private license, you could reference
+to LICENSE-WINIO for more detail.
+
+We only included part of WinIo binaries, the full WinIo package with help file,
+sources, binaries could be download from www.internals.com, or my backup fork
+https://github.com/starofrainnight/winio if internals's website is down.
+
---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file, open('HISTORY.rst') as history_file:
+    long_description = (readme_file.read() + "\n\n" + history_file.read())
+
+requirements = [
+    'click>=6.0',
+    # TODO: put package requirements here
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(starofrainnight): put setup requirements (distutils extensions,
+    # etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='winiobinary',
+    version='0.0.1',
+    description="WinIo's binary python package that support install from pypi",
+    long_description=long_description,
+    author="Hong-She Liang",
+    author_email='starofrainnight@gmail.com',
+    url='https://github.com/starofrainnight/winiobinary',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License",
+    zip_safe=False,
+    keywords='winiobinary',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
