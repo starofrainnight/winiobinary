729ebbdfa923ab0784a6c062edcdd9f2e5eaac0b
Hong-She Liang <starofrainnight@gmail.com>
2018-01-08 21:52:40 +0800

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='winiobinary',
-    version='0.0.2',
+    version='0.0.3',
     description="WinIo's binary python package that support install from pypi",
     long_description=long_description,
     author="Hong-She Liang",
