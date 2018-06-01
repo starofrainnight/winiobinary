d688cb9fc1f026755de32614e3d1ffc5805f2644
Hong-She Liang <starofrainnight@gmail.com>
2018-06-01 17:30:15 +0800

    Fixed comment line too long

--- winiobinary/__init__.py
+++ winiobinary/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 #
-# Copyright 2018, Hong-She Liang <starofrainnight@gmail.com>.  All rights reserved.
+# Copyright 2018, Hong-She Liang <starofrainnight@gmail.com>.  All rights
+# reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
